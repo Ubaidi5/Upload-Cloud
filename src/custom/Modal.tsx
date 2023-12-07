ec96ab88d132ba31ff52c2bf311a47936ead9bf3
ubaidi5 <ubaid7625@gmail.com>
2023-12-07 23:35:39 +0500

    Recreate modal with React Portal

--- src/custom/Modal.tsx
+++ src/custom/Modal.tsx
@@ -1,59 +1,30 @@
-import styled from "styled-components";
+"use client";
+import { createPortal } from "react-dom";
+import CancelIcon from "@public/icons/cancel.svg";
 
-interface ModalProps {
+type Props = {
   open: boolean;
   onCancel: () => void;
   children: React.ReactNode;
-  closeable?: boolean;
-  width?: number | string;
-}
-
-const Modal: React.FC<ModalProps> = (props) => {
-  const { open, onCancel, children, closeable = true, width = 600 } = props;
-
-  if (open === false) {
-    return;
-  }
-
-  return (
-    <StyledModal>
-      <div
-        className="modal-mask"
-        onClick={() => {
-          if (closeable) {
-            onCancel();
-          }
-        }}
-      >
-        <div
-          className="modal-wrapper"
-          style={{ width: width }}
-          onClick={(e) => e.stopPropagation()}
-        >
-          {children}
-        </div>
-      </div>
-    </StyledModal>
-  );
+  bgColor?: string;
+  style?: React.CSSProperties;
+  className?: string;
 };
 
+const Modal: React.FC<Props> = ({ open, children, bgColor = "#fff", onCancel, style, className }) =>
+  open &&
+  createPortal(
+    <>
+      <div className="mask" onClick={onCancel} />
+      <div className="modal-wrapper">
+        <div className={`modal ${className}`} style={style ? style : undefined}>
+          <div className="modal-content" style={{ background: bgColor }}>
+            {children}
+          </div>
+        </div>
+      </div>
+    </>,
+    document.body
+  );
+
 export default Modal;
-
-const StyledModal = styled("div")`
-  .modal-mask {
-    position: fixed;
-    inset: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    z-index: 2147483640;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .modal-wrapper {
-    background-color: #fff;
-    /* padding: 16px; */
-    border-radius: 4px;
-    z-index: 2147483647;
-  }
-`;
