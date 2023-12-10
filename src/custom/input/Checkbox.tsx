03b9dc36b1937014e623030a2712047559ea9907
ubaidi5 <ubaid7625@gmail.com>
2023-12-10 20:47:31 +0500

    change some styles in checkbox

--- src/custom/input/Checkbox.tsx
+++ src/custom/input/Checkbox.tsx
@@ -11,22 +11,26 @@ interface CheckboxProps {
 }
 
 const Checkbox: React.FC<CheckboxProps> = (props) => {
-  const { checked, onChange, color = "#6338FA", size = 20, children } = props;
+  const { checked, onChange, color = "#6338FA", size = 18, children } = props;
   return (
     <div className="flex items-center gap-2" onClick={() => onChange?.(!checked)} role="button">
-      <span
-        style={{
-          background: checked ? color : "transparent",
-          width: size,
-          height: size,
-          borderRadius: 3,
-          border: `1px solid ${color}`,
-        }}
-        className="flex items-center jc-center"
-      >
-        {checked ? <CheckIcon style={{ color: "#fff", width: size - 6 }} /> : null}
+      <span style={{ padding: 2 }}>
+        <span
+          style={{
+            background: checked ? color : "transparent",
+            width: size,
+            height: size,
+            borderRadius: 3,
+            border: `1px solid ${color}`,
+          }}
+          className="flex items-center justify-center"
+        >
+          {checked ? <CheckIcon style={{ color: "#fff", width: size - 6 }} /> : null}
+        </span>
       </span>
-      <span style={{ userSelect: "none" }}>{children}</span>
+      <span style={{ userSelect: "none" }} className="fs-14 checkbox-label">
+        {children}
+      </span>
     </div>
   );
 };
