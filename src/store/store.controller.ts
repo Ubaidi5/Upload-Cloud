fb635ee91f334065dde9e510bb1d9f6bb8e80ddb
ubaidi5 <ubaid7625@gmail.com>
2023-12-15 01:04:08 +0500

    working on store apis

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -1,13 +1,40 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { StoreService } from './store.service';
 
 @Controller('store')
 export class StoreController {
   constructor(private storeService: StoreService) {}
 
-  @Post('')
-  create_store() {}
+  @Post('install')
+  install(@Body() code: string) {
+    return this.storeService.install(code);
+  }
 
-  @Get('instance')
-  get_store_instance() {}
+  @Get('load')
+  async load_store(@Body() instanceId: string) {
+    // return this.storeService.load_store(instanceId);
+    try {
+      // get store
+      const store = await this.storeService.get_store_by_instanceId(instanceId);
+
+      // get access_token
+      const access_token = await this.storeService.get_access_token(store.refreshToken);
+
+      // get store instance
+      const app = await this.storeService.get_app_instance(access_token);
+
+      return {
+        store,
+        site: app.site,
+        instance: app.instance,
+      };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  @Post('embed_script')
+  async embed_script(@Body() instanceId: string) {
+    // const access_token
+  }
 }
--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -5,7 +5,100 @@ import { DBService } from 'src/db/db.service';
 export class StoreService {
   constructor(private db: DBService) {}
 
-  create_store() {}
+  async install(code: string) {
+    try {
+      const OAuthRequest = {
+        method: 'POST',
+        body: JSON.stringify({
+          grant_type: 'authorization_code',
+          client_id: process.env.APP_ID,
+          client_secret: process.env.APP_SECRET,
+          code: code,
+        }),
+      };
 
-  get_store_instance() {}
+      const data = await (await fetch('https://www.wixapis.com/oauth/access', OAuthRequest)).json();
+
+      const appInstanceRequest = {
+        method: 'GET',
+        headers: { Authorization: data.access_token },
+      };
+
+      const app = await (
+        await fetch('https://www.wixapis.com/apps/v1/instance', appInstanceRequest)
+      ).json();
+
+      const body = {
+        siteUrl: app.site.url,
+        instanceId: app.instance.instanceId,
+        refreshToken: data.refresh_token,
+      };
+
+      await this.db.stores.findOneAndUpdate(
+        { instanceId: body.instanceId },
+        { $set: { ...body } },
+        { upsert: true, new: true, setDefaultsOnInsert: true },
+      );
+
+      return data.access_token;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async get_store_by_instanceId(instanceId: string) {
+    return await this.db.stores.findOne({ instanceId: instanceId });
+  }
+
+  async get_access_token(refreshToken: string) {
+    try {
+      const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({
+          grant_type: 'refresh_token',
+          client_id: process.env.APP_ID,
+          client_secret: process.env.APP_SECRET,
+          refresh_token: refreshToken,
+        }),
+      };
+
+      const response = await fetch('https://www.wixapis.com/oauth/access', requestOptions);
+      const data = await response.json();
+
+      return data.access_token;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async get_app_instance(access_token: string) {
+    const requestOptions = {
+      method: 'GET',
+      headers: { Authorization: access_token },
+    };
+    const response = await fetch('https://www.wixapis.com/apps/v1/instance', requestOptions);
+    const app = response.json();
+    return app;
+  }
+
+  async load_store(instanceId: string) {
+    try {
+      // get store
+      const store = await this.db.stores.findOne({ instanceId: instanceId });
+
+      // get access_token
+      const access_token = await this.get_access_token(store.refreshToken);
+
+      // get app instance
+      const app = await this.get_app_instance(access_token);
+
+      return {
+        store,
+        instance: app.instance,
+        site: app.site,
+      };
+    } catch (err) {
+      throw err;
+    }
+  }
 }
