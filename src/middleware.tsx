5a7143bc8d5fc5edd55bfbb8494f5080c4437b5c
ubaidi5 <ubaid7625@gmail.com>
2023-12-19 20:16:52 +0500

    complete installation

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -9,16 +9,45 @@ export async function middleware(request: NextRequest) {
     },
   });
 
-  if (request.nextUrl.pathname === "/first") {
+  if (request.nextUrl.pathname === "/token") {
     const token = request.nextUrl.searchParams.get("token");
     const appId = process.env.NEXT_PUBLIC_APP_ID;
+
     const redirect_url =
       process.env.NODE_ENV === "development"
-        ? "http://localhost:3000/install"
+        ? "https://upload-cloud.loca.lt/install"
         : `${process.env.NEXT_PUBLIC_BACKEND_URL}/install`;
 
     return NextResponse.redirect(
       `https://www.wix.com/installer/install?token=${token}&appId=${appId}&redirectUrl=${redirect_url}`
+    );
+  }
+
+  if (request.nextUrl.pathname === "/install") {
+    const code = request.nextUrl.searchParams.get("code");
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code }),
+    };
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/store/install`,
+      requestOptions
+    );
+    const data = await res.json();
+
+    console.log({ data });
+
+    await fetch("https://www.wixapis.com/apps/v1/bi-event", {
+      method: "POST",
+      headers: { Authorization: data.access_token },
+      body: JSON.stringify({ eventName: "APP_FINISHED_CONFIGURATION" }),
+    });
+
+    return NextResponse.redirect(
+      `https://www.wix.com/installer/close-window?access_token=${data.access_token}`
     );
   }
 
