34763e61b363f9b728a85c4a554545b4482dbffc
ubaidi5 <ubaid7625@gmail.com>
2023-12-19 22:57:28 +0500

    add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { FieldModule } from './field/field.module';
 import { StoreModule } from './store/store.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 const mongo_url = 'mongodb://localhost:27017/upload-field';
 
 @Module({
-  imports: [MongooseModule.forRoot(mongo_url), StoreModule, FieldModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(mongo_url),
+    StoreModule,
+    FieldModule,
+  ],
 })
 export class AppModule {}
