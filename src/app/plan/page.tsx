831e034794f1ff2a15c616f888839cfb51a21404
ubaidi5 <ubaid7625@gmail.com>
2023-12-21 23:05:51 +0500

    change app name, logo and teaser in plan page

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -4,11 +4,17 @@ function Plan() {
   return (
     <main>
       <section className="py-3">
-        <div className="card p-2 flex items-center gap-8">
-          <img src="/images/fav-icon-light.svg" width={56} height={56} alt="Photo Comet" />
+        <div className="card p-2 flex items-center gap-3">
+          <img
+            src="/images/app-icon-light.svg"
+            width={56}
+            height={56}
+            alt="Upload Cloud"
+            style={{ borderRadius: 12 }}
+          />
           <div>
-            <h2 className="fc-light fw-800">Photo Comet</h2>
-            <p className="fc-dark fs-13">Upload Image & Customize Order with Comet Base</p>
+            <h1 className="fs-24 fc-light fw-800">Upload Cloud</h1>
+            <p className="fc-dark fs-13">Enable user to attach Images in orders!</p>
           </div>
         </div>
       </section>
