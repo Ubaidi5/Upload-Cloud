2e1f5a894b466aa9f9a878989019fbb2a8a389d3
ubaidi5 <ubaid7625@gmail.com>
2023-12-21 23:31:02 +0500

    change spinner size in loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="flex items-center justify-center" style={{ height: "100vh" }}>
       <div className="text-center">
-        <Spinner className="mb-2" color="#6338FA" />
+        <Spinner className="mb-2" width={64} stroke={4} />
         <p className="fc-light fs-16 fw-600">Please wait...</p>
       </div>
     </div>
