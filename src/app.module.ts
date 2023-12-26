e6a6bbd5fc9c56770ae74dbeac773e8bb730a586
ubaidi5 <ubaid7625@gmail.com>
2023-12-26 17:51:41 +0500

    change  mongodb connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { StoreModule } from './store/store.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
-const mongo_url = 'mongodb://localhost:27017/upload-field';
+const mongo_url =
+  'mongodb://ubaid-admin:dbc7a8832c7447118e25d0ee49ecd168@localhost:28185/upload-field';
 
 @Module({
   imports: [
