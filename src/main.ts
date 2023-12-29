78c0163d539c6f3c352b970e85fddd8c444ffcd4
ubaidi5 <ubaid7625@gmail.com>
2023-12-30 01:34:01 +0500

    add some code to read webhook data

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.use(function (req, res, next) {
+    if (!req.path.includes('webhook')) {
+      return next();
+    }
+
+    let data = '';
+    req.on('data', function (chunk) {
+      data += chunk;
+    });
+    req.on('end', function () {
+      req.digest = data;
+      next();
+    });
+  });
+
   await app.listen(3002);
 }
 bootstrap();
