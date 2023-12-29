fc1ea0b8d46203e8c418e1b84aaa043bd1b0129f
ubaidi5 <ubaid7625@gmail.com>
2023-12-29 21:15:47 +0500

    resolved mongodb issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { OrderModule } from './order/order.module';
 
-const mongo_url = 'mongodb://localhost:27017/upload-field';
+const mongo_url =
+  'mongodb://ubaid-admin:dbc7a8832c7447118e25d0ee49ecd168@localhost:28185/upload-field?authSource=admin';
 
 @Module({
   imports: [
