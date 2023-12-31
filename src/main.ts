16375ba89415e7678ba93d97ac71725cf8b87ced
ubaidi5 <ubaid7625@gmail.com>
2023-12-31 17:38:30 +0500

    misc.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { urlencoded, json } from 'express';
-import * as bodyParser from 'body-parser';
+// import { urlencoded, json } from 'express';
+// import * as bodyParser from 'body-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  // app.use(json({ limit: '50mb' }));
+  // app.use(urlencoded({ extended: true, limit: '50mb' }));
+  // app.use(bodyParser.json({ limit: '50mb' }));
+  // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  // app.use(bodyParser.raw({ limit: '50mb' }));
+  app.useBodyParser('urlencoded', { limit: '50mb' });
+  app.useBodyParser('text', { limit: '50mb' });
+  app.useBodyParser('raw', { limit: '50mb' });
 
   app.use(function (req, res, next) {
     if (!req.path.includes('webhook')) {
